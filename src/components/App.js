fb964eeec7aaff55533f42c2be786e1b52a5a5af
María Sánchez <msanchezroman95@gmail.com>
2023-06-26 08:43:39 +0200

    add react starter kit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import '../styles/App.scss';
+
+//https://beta.adalab.es/curso-intensivo-fullstack-recursos/apis/quotes-friends-tv-v1/quotes.json
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>Frases de Friends</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
